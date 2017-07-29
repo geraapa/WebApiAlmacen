5b18c8e164e32d051266f1c45a9a11a8d859fe9f
geraapa <geraapa@gmail.com>
2017-07-29 12:46:59 -0500

    WebApiAlmacenV2

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -9,6 +9,13 @@ namespace WebApiAlmacen
     {
         public static void Register(HttpConfiguration config)
         {
+            //Evito las referencias circulares al trabajar con Entity FrameWork         
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            //Elimino que el sistema devuelva en XML, sólo trabajaremos con JSON
+            config.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             // Configuración y servicios de API web
 
             // Rutas de API web
